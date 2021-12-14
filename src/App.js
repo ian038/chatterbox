8ddda181e0038e6664216668f981c55a615e9df9
ian038 <ianphua3@gmail.com>
2021-12-13 19:10:01 -0500

    Successfully completed auth

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from "react"
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
-// import { AuthProvider } from "../contexts/AuthContext"
+import { AuthProvider } from "./contexts/AuthContext"
 
-// import Chats from "./components/Chats"
+import Chats from "./components/Chats"
 import Login from "./components/Login"
 
 function App() {
   return (
     <div style={{ fontFamily: 'Avenir' }}>
       <Router>
-        {/* <AuthProvider> */}
+        <AuthProvider>
           <Switch>
-            {/* <Route path="/chats" component={Chats} />*/}
+            <Route path="/chats" component={Chats} />
             <Route path="/" component={Login} />
           </Switch>
-        {/* </AuthProvider> */}
+        </AuthProvider>
       </Router>
     </div>
   )
